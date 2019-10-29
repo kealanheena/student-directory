86bb6222562b0eb5d46301f7f8828ea1960f9915
kealanheena <kealanheena@gmail.com>
2019-10-29 17:27:58 +0000

    add part 9 use 'student' instead of 'students' if there is only one student

--- directory.rb
+++ directory.rb
@@ -1,50 +1,98 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
+  # create a hash with empty arrays for each cohort
+  @cohorts = {
+  January: [],
+  February: [],
+  March: [],
+  April: [],
+  May: [],
+  June: [],
+  July: [],
+  August: [],
+  September: [],
+  October: [],
+  November: [],
+  December: []
+  }
   # get the first name
   name = gets.chomp
   # while name is not empty, repeat this code
   while !name.empty? do
-    # dont know how to check for typo come back to it later
-    puts "Please enter the students cohort"
-    cohort = gets.chomp
-    cohort.empty? ? (cohort = "november") : (cohort.to_sym)
-    puts "Please enter the students age"
-    # adding more info works the same
-    age = gets.chomp
-    age = "N/A" if age.empty?
-    # add the student hash to the array
-    students << {name: name, age: age, cohort: cohort}
-    puts "We now have #{students.count} students"
+    cohort = input_cohort
+    age = input_age
+    add_to_cohort(name, age, cohort)
     #get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
+  @cohorts
 end
+
+def input_cohort
+  puts "Please enter the students cohort"
+  # takes user input and makes first letter a capital for compairing to cohorts values
+  cohort = gets.chomp.capitalize
+  # sets cohort to november if the user doesnt choose a cohort
+  cohort = "November" if cohort.empty?
+  cohort
+end
+
+def input_age
+  puts "Please enter the students age"
+  # adding more info works the same
+  # gets user input for students age
+  age = gets.chomp
+  # sets age to N/A if user doesn't set an age
+  age = "N/A" if age.empty?
+  age
+end
+
+def add_to_cohort(name, age, cohort)
+  @cohorts.each { |key, value|
+    if cohort.include?(key.to_s[0..2])
+      # add the student hash to the array
+      value << {name: name, age: age}
+    end
+  }
+  head_count = @cohorts.map {|key, value| value.count}.reduce(0, :+)
+  head_count == 1 ? (puts "We now have #{head_count} student") : (puts "We now have #{head_count} students")
+  @cohorts
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "--------------------------------"
 end
+
 def print(students)
   # students.each_with_index do |student, index|
   #   if student[:name][0].downcase == "k" && student[:name].length < 12
   #     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   #   end
   # end
-  i = 0
-  while i < students.length
-    puts "#{i + 1}. #{students[i][:name]} age: #{students[i][:age]} (#{students[i][:cohort]} cohort)".center(40, "-")
-    i += 1
+  # i = 0
+  # while i < students.length
+  #   puts "#{i + 1}. #{students[i][:name]} age: #{students[i][:age]} (#{students[i][:cohort]} cohort)".center(40, "-")
+  #   i += 1
+  # end
+  puts "Which cohort would you like to see?"
+  selected_cohort = gets.chomp.capitalize
+  students.each do |key, value|
+    if selected_cohort.include?(key.to_s[0..2])
+      puts "#{key} cohorts students:"
+      value.each_with_index do |student, index|
+        puts "#{index + 1}. #{student[:name]} age: #{student[:age]}"
+      end
+    end
   end
 end
+
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  head_count = students.map {|key, value| value.count}.reduce(0, :+)
+  head_count == 1 ? (puts "Overall, we have #{head_count} great student") : (puts "Overall, we have #{head_count} great students")
 end
 
-# nothing happens untill we call the methods
 students = input_students
 print_header
 print(students)
