f61c53c02d6b1f83cdb57e8d200b3ebd51d98b86
kealanheena <kealanheena@gmail.com>
2019-10-29 01:25:11 +0000

    complete exercises 1-6

--- directory.rb
+++ directory.rb
@@ -1,14 +1,21 @@
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit retuen twice"
+  puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
   # while name is not empty, repeat this code
   while !name.empty? do
+    puts "Please enter the students cohort"
+    cohort = gets.chomp
+    cohort.empty? ? (cohort = "november") : (cohort.to_sym)
+    puts "Please enter the students age"
+    # adding more info works the same
+    age = gets.chomp
+    age = "N/A" if age.empty?
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, age: age, cohort: cohort}
     puts "We now have #{students.count} students"
     #get another name from the user
     name = gets.chomp
@@ -21,8 +28,15 @@ def print_header
   puts "--------------------------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  # students.each_with_index do |student, index|
+  #   if student[:name][0].downcase == "k" && student[:name].length < 12
+  #     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  #   end
+  # end
+  i = 0
+  while i < students.length
+    puts "#{i + 1}. #{students[i][:name]} age: #{students[i][:age]} (#{students[i][:cohort]} cohort)".center(40, "-")
+    i += 1
   end
 end
 def print_footer(students)
